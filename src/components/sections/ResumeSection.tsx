03b8c32104db88686474c7ffb3de42f14269b266
Priyanshu <priyanshumails.bca2025@gmail.com>
2025-12-30 08:19:12 +0530

    update post graduation enthusiatic

--- src/components/sections/ResumeSection.tsx
+++ src/components/sections/ResumeSection.tsx
@@ -10,7 +10,7 @@ const ResumeSection = () => {
       institution: "Rustamji Institute of Technology",
       location: "Tekanpur Gwalior, Madhya Pradesh",
       year: "2025 - 2027",
-      enthusiastic: ["AI Tools","Data Sciences"]
+      enthusiastic: ["Exploring AI Tools"]
     },
     {
       level: "Graduated",
