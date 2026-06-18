f86110d4057f8d3d209642b6225778e7f0ce5a05
Priyanshu Kumar <priyanshumails.bca2025@gmail.com>
2026-06-18 15:43:57 +0530

    update resume with mail id

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -23,7 +23,7 @@ const HeroSection = () => {
 
  const handleDownloadCV = () => {
   // Convert your Google Drive link to a direct download link
-  const resumeUrl = 'https://drive.google.com/file/d/1bAdUOMc1IaBPFiCzFUBmwm_Kp_wpr1Pj/view?usp=drive_link';
+  const resumeUrl = 'https://drive.google.com/file/d/19rzSNZJx8WO5pdhCBG9utYVOqnQdb4Ul/view?usp=sharing';
 
   // Create a temporary link element to trigger the file download
   const link = document.createElement('a');
